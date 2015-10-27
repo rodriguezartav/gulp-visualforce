cac64aba00f3ed1a2f37da21c63fb6d104434959
Roberto Rodriguez <roberto@rodcocr.com>
2015-10-26 18:17:17 -0600

    add offline

--- example/gulp/tasks/markup.js
+++ example/gulp/tasks/markup.js
@@ -1,12 +1,13 @@
 var gulp = require('gulp');
 var notify = require("gulp-notify");
-
+var minimist = require("minimist")
+var args = minimist( process.argv );
 var handleErrors = require('../util/handleErrors');
-
 
 var dest = './build';
 
 gulp.task('markup', function() {
+
   return gulp.src(['src/htdocs/**','!src/htdocs/*.html'])	
    .pipe(gulp.dest(dest));
 });
--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
 
 	Visualforce: require("./lib/transform"),
 	StaticResourcePack: require("./lib/staticresource"),
-	BrowserSync: require("./lib/browserSync")
+	BrowserSync: require("./lib/browserSync"),
+	Clay: require("./lib/clay")
 }
